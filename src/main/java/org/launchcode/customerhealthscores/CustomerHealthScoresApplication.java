2f0e9a2e8e01414dd0978204fa12d9a6ea6fc248
NoahPG88 <57874889+NoahPG88@users.noreply.github.com>
2020-08-13 22:04:21 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/customerhealthscores/CustomerHealthScoresApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.customerhealthscores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerHealthScoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerHealthScoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/customerhealthscores/CustomerHealthScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.customerhealthscores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerHealthScoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
